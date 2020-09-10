27cc3f445a91a93dec845299e90fc192f942bb00
JayantBenjamin <benja121@msu.edu>
2020-09-10 01:47:48 -0400

    Initial Commit

---
+++ Tip3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip3
+//
+//  Created by Pratibha Benjamin on 10/09/20.
+//  Copyright © 2020 Jayant Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
